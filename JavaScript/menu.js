c16e048cfdd9fd16cef93553fadd3e244fb0f223
SailTheSeas <2430921@students.wits.ac.za>
2024-04-24 10:41:54 +0200

    JavaScript 1

---
+++ JavaScript/menu.js
@@ -0,0 +1,31 @@
+// JavaScript source code
+const root = "/WSOA3028_2430921"
+
+const menuItems =
+    [
+        { name: "Home", href: root + "index/html" },
+        { name: "Blog", href: root + `${root}/Blogs/index.html` },
+        { name: "Essays", href: root + `${root}/Essays/index.html` },
+        { name: "Portfolio", href: root + `${root}/Portfolio/index.html` },
+        { name: "Design", href: root + `${root}/Designs/index.html` },
+
+
+
+    ];
+
+export function initialize(currentPage) {
+    var nav = document.querySelector("header > nav")
+    var ul = document.createElement("ul")
+
+    for (let menuItem of menuItems) {
+        if (currentPage != menuItem.name) {
+            var li = document.createElement("li")
+            var a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+            ul.appendChild(li)
+        }
+    }
+    nav.appendChild(ul)
+}
