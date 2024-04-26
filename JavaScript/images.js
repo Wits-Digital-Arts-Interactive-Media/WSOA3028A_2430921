76a78e42cdaec312cc0b215d60a19b78adb52194
SailTheSeas <2430921@students.wits.ac.za>
2024-04-26 23:25:47 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -9,22 +9,23 @@ export function initialise(folder, numOfPics) {
     const img = [
         document.createElement('img'),
     ]
+        
+        /*
     img[0].src = (`Animals/Animals1.jpg`)
     img[0].width = 400
     img[0].height = 400
     document.body.appendChild(img[0])
-
-    /*
+    */
+    
     for (var i = 1; i <= numOfPics; i++) {
-        img[i].src = (`/WSOA3028A_2430921/Portfolio/Pictures/${folder}/${folder}${i}.jpg`)
-        img[i].width = 400
-        img[i].height = 400
-        //img[i].width = img[i].naturalHeight/4
-        //img[i].height = img[i].naturalWidth/4
+       
+        img[i].src = (`${folder}/${folder}${i}.jpg`)
+        img[i].width = img[i].naturalHeight/4
+        img[i].height = img[i].naturalWidth/4
         document.body.appendChild(img[i])
         img.push(document.createElement('img'))
     }
-    */
+    
 
 
 }
