23a42895f54560d1601fd964b049f9bfaa937c18
SailTheSeas <2430921@students.wits.ac.za>
2024-04-26 07:29:18 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -14,7 +14,9 @@ export function initialise() {
     img[0].src = ("WSOA3028A_2430921/Portfolio/Pictures/Landscape1.jpg")
     img[0].width = 300
     img[0].height = 100
-    img[1].src = ("WSOA3028A_2430921\Portfolio\Pictures\Landscape2.jpg")
+    img[1].src = ("WSOA3028A_2430921/Portfolio/Pictures/Landscape2.jpg")
+    img[1].width = 300
+    img[1].height = 100
     // Append the img element to the document body or another element 
     document.body.appendChild(img[0]);
     document.body.appendChild(img[1]);
