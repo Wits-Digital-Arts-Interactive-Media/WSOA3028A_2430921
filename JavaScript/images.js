26e6afc920a4e634c8564a33957577f518f5c4e6
SailTheSeas <2430921@students.wits.ac.za>
2024-04-26 07:01:34 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -12,6 +12,8 @@ export function initialise() {
     img.push(document.createElement('img2'))
     // Set the src attribute to the URL of the image 
     img[0].src = ("WSOA3028A_2430921\Portfolio\Pictures\Landscape1.jpg")
+    img[0].width = 300
+    img[0].height = 100
     img[1].src = ("WSOA3028A_2430921\Portfolio\Pictures\Landscape2.jpg")
     // Append the img element to the document body or another element 
     document.body.appendChild(img[0]);
