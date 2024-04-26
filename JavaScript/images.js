29652b280ce875b1d6871a53440bbd9593946a1b
SailTheSeas <2430921@students.wits.ac.za>
2024-04-26 21:34:56 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -4,20 +4,20 @@ const root = "/WSOA3028A_2430921/Portfolio"
 
 
 
-export function initialise() {
+export function initialise(folder, numOfPics) {
 
     const img = [
         document.createElement('img'),
     ]
-    img.push(document.createElement('img'))
-    // Set the src attribute to the URL of the image 
-    img[0].src = ("WSOA3028A_2430921/Portfolio/Pictures/Landscape1.jpg")
-    img[0].width = 300
-    img[0].height = 100
-    img[1].src = ("WSOA3028A_2430921/Portfolio/Pictures/Landscape2.jpg")
-    img[1].width = 300
-    img[1].height = 100
+
+
+    for (var i = 0; i < numOfPics; i++) {
+        img[i].src = (`/WSOA3028A_2430921/Portfolio/Pictures/${folder}/${folder}${i}.jpg`)
+        img[i].width = img[i].naturalHeight/4
+        img[i].height = img[i].naturalWidth/4
+        document.body.appendChild(img[i])
+        img.push(document.createElement('img'))
+    }
     // Append the img element to the document body or another element 
-    document.body.appendChild(img[0]);
-    document.body.appendChild(img[1]);
+
 }
