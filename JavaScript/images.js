b735ce52dac52b18cda425ac937d1900ee82d8ad
SailTheSeas <2430921@students.wits.ac.za>
2024-04-27 21:01:23 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -20,13 +20,13 @@ export function initialise(folder, numOfPics) {
     for (var i = 1; i <= numOfPics; i++) {
        
         img[i - 1].src = (`${folder}/${folder}${i}.jpg`)
-        img[i - 1].width = 400
-        img[i - 1].height = 400
+        /*img[i - 1].width = 400
+        img[i - 1].height = 400*/
         
-        console.log("Originals: " + img[i - 1].naturalHeight + " " + img[i - 1].naturalWidth)
-        console.log("Fake: " + img[i - 1].height + " " + img[i - 1].width)
-        /*img[i-1].width = img[i-1].naturalHeight/4
-        img[i-1].height = img[i-1].naturalWidth/4*/
+        /*console.log("Originals: " + img[i - 1].naturalHeight + " " + img[i - 1].naturalWidth)
+        console.log("Fake: " + img[i - 1].height + " " + img[i - 1].width)*/
+        img[i-1].width = img[i-1].naturalHeight/4
+        img[i-1].height = img[i-1].naturalWidth/4
         document.body.appendChild(img[i-1])
         img.push(document.createElement('img'))
     }
