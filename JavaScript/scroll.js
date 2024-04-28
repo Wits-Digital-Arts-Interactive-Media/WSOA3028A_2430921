1f802a275f487260b35cfcce53940c1fbe2f1950
SailTheSeas <2430921@students.wits.ac.za>
2024-04-28 21:10:52 +0200

    JS

--- JavaScript/scroll.js
+++ JavaScript/scroll.js
@@ -3,3 +3,14 @@
 function ScrollUp() {
     window.scrollTo(0, 0);
 }
+
+export function initialise() {
+
+
+
+    const button = document.createElement('button')
+    button.textContent = 'To Top'
+    document.body.appendChild(newBubuttontton)
+
+    button.addEventListener('click', ScrollUp);
+}
