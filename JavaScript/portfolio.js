8a653730758b968e02b845856a1219a35b9b4f30
SailTheSeas <2430921@students.wits.ac.za>
2024-04-28 18:38:19 +0200

    JS

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -2,23 +2,23 @@
 
 const root = "/WSOA3028A_2430921/Portfolio"
 
-const portfolioItems =
-    [
-        "Animals",
-        "Architecture",
-        "Landscapes",
-        "Moon",
-        "Nature",
-        "Portraits",
-        "Sun",
-    ]
+
 
 export function initialise(numOfPics) {
 
     const img = [
         document.createElement('img'),
     ]
-
+    const portfolioItems =
+        [
+            "Animals",
+            "Architecture",
+            "Landscapes",
+            "Moon",
+            "Nature",
+            "Portraits",
+            "Sun",
+        ]
 
     for (var i = 1; i <= numOfPics; i++) {
 
