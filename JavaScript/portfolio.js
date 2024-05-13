19ec11ae8fda38debeeda3e5930e2449cb563514
SailTheSeas <2430921@students.wits.ac.za>
2024-05-13 15:48:37 +0200

    CSS

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -2,16 +2,7 @@
 
 const root = "/WSOA3028A_2430921/Portfolio"
 
-/*const portfolioItems =
-    [
-        { name: "Animals", href: `Animals/portfolio.html`},
-        { name: "Architecture", href: `Architecture/portfolio.html` },
-        { name: "Landscapes", href: `Landscapes/portfolio.html` },
-        { name: "Moon", href: `Moon/portfolio.html` },
-        { name: "Nature", href: `Nature/portfolio.html` },
-        { name: "Portraits", href: `Portraits/portfolio.html` },
-        { name: "Sun", href: `Sun/portfolio.html` },
-    ];*/
+
 const portfolioItems =
     [
         { name: "Animals", href: `Animals/portfolio.html` },
@@ -43,41 +34,55 @@ export function initialise(numOfPics) {
     ]
 
 
+    const div = [
+        document.createElement('div')
+    ]
+
+
     for (var i = 1; i <= numOfPics; i++) {
 
-            
-
-            img[i - 1].src = (portfolioPictures[i-1].href)
-
+                
+        var container = document.createElement('div')
+        container.classList.add('container')
+        img[i - 1].src = (portfolioPictures[i - 1].href);
+        img[i - 1].name = (portfolioPictures[i - 1].name);
+        div[i - 1].textContent = portfolioPictures[i - 1].name;
+        div[i - 1].id = portfolioPictures[i - 1].name; 
             /*img[i - 1].width = img[i - 1].naturalWidth / 8
             img[i - 1].height = img[i - 1].naturalHeight / 8
             */
 
             
-            img[i-1].setAttribute("value",i-1)
-            img[i - 1].classList.add('link')
-            document.body.appendChild(img[i - 1])
+        img[i - 1].setAttribute("value", i - 1);
+        img[i - 1].classList.add('link');
+        div[i - 1].classList.add('label')
+        container.appendChild(img[i - 1]);
+        container.appendChild(div[i - 1]);
+        document.body.appendChild(container);
+        
 
 
-            img.push(document.createElement('img'))
-            img[i - 1].onclick = function () {
-                window.location.href = portfolioItems[this.getAttribute("value")].href;
-                console.log(this.getAttribute("value"));
+        img.push(document.createElement('img'));
+        div.push(document.createElement('div'));
+        img[i - 1].onclick = function () {
+            window.location.href = portfolioItems[this.getAttribute("value")].href;
+            console.log(this.getAttribute("value"));
 
  
         };
 
         img[i - 1].onmouseenter = function (event) {
-                //console.log( this.width + " " + this.height)
-                this.width = this.width + 50 
-                this.height = this.height + 50
+            //console.log( this.width + " " + this.height)
+            var text = document.getElementsByName(this.name)
+            text.style.display = 'inline';
         };
 
         img[i - 1].onmouseleave = function (event) {
-                //console.log( this.width + " " + this.height)
-                this.width = this.width - 50
-                this.height = this.height - 50
+            //console.log( this.width + " " + this.height)
+            var text = document.getElementsByName(this.name)
+            text.style.display = 'none';
         };
+
 
 
     }
