448fc3322e19bc559d30d21bd78aaec5c97b8916
SailTheSeas <2430921@students.wits.ac.za>
2024-05-14 20:35:33 +0200

    CSS + JS

--- JavaScript/sticky.js
+++ JavaScript/sticky.js
@@ -1,14 +1,13 @@
 
 
-    var header = document.getElementById('menu');
-    window.addEventListener("scroll", function() {
+    //var header = document.getElementById('menu');
+var header = document.querySelector('nav');
+window.addEventListener("scroll", function () {
     
     
     //console.log(offSet);
     console.log('Y:' + scrollY);
     console.log('Top:' + document.documentElement.scrollTop)
-    var currentdate = new Date();
-    console.log(currentdate.getSeconds());
     if (window.scrollY > 0) {
         console.log("scroll");
         header.classList.add("sticky");
