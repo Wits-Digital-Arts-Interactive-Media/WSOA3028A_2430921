0ea5c71305398ccec1cfe1db7b4be22ace4f6726
SailTheSeas <2430921@students.wits.ac.za>
2024-05-15 10:31:18 +0200

    CSS

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -51,26 +51,3 @@ export function initialise(currentPage) {
     nav.appendChild(div)
     nav.appendChild(document.createElement("hr"))
 }
-
-/*
-export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav")
-    const ul = document.createElement("ul")
-
-    for (let menuItem of menuItems) {
-        const li = document.createElement("li")
-        if (currentPage != menuItem.name) {
-            const a = document.createElement("a")
-            a.innerText = menuItem.name
-            a.setAttribute("href", menuItem.href)
-            li.appendChild(a)
-           
-        } else (li.innerText = menuItem.name)
-        ul.appendChild(li)
-
-        
-    }
-    nav.appendChild(ul)
-    nav.appendChild(document.createElement("hr"))
-}
-*/
