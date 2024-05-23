85c3c00de72d05eebdbddc9e0e8eb02e20fc66c5
SailTheSeas <2430921@students.wits.ac.za>
2024-05-23 21:21:30 +0200

    CSS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -5,38 +5,19 @@ const root = "/WSOA3028A_2430921/Portfolio"
 
 
 export function initialise(folder, numOfLPics, numOfPPics) {
-
-    /*
-    const img = [
-        document.createElement('img'),
-    ]*/
-
-    const wideImg = [
-        document.createElement('img'),
-    ]
-    const tallImg = [
-        document.createElement('img'),
-    ]
-
-    var wideCount = 0
-    var tallCount = 0
-        /*
-    img[0].src = (`Animals/Animals1.jpg`)
-    img[0].width = 400
-    img[0].height = 400
-    document.body.appendChild(img[0])
-    */
     
     for (var i = 1; i <= numOfLPics; i++) {
 
         const img = document.createElement('img')
         img.src = (`Landscape/${folder} (${i}).jpg`)
+        img.classList.add('display');
         document.body.appendChild(img);
     }
     for (var i = 1; i <= numOfPPics; i++) {
 
         const img = document.createElement('img')
         img.src = (`Portrait/${folder} (${i}).jpg`)
+        img.classList.add('display');
         document.body.appendChild(img);
     }
 
