38364ee15580452c958593aee430911a451fc7e2
SailTheSeas <2430921@students.wits.ac.za>
2024-05-24 21:21:42 +0200

    JS

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -76,7 +76,7 @@ export function initialise(folder, numOfLPics, numOfPPics) {
     for (var i = 0; i < count; i++) {
         images[i].id = i;
         images[i].addEventListener("click", function () { openGallery(); showSlides(images[i].id)});
-        images[i].para
+         
             /*openGallery ();*//*, currentSlide(i+1);*/
     }
     showSlides(slideIndex);
