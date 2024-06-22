f21150aeb4f40de895acfd47ab2b48ffa94761b8
SailTheSeas <2430921@students.wits.ac.za>
2024-06-22 12:40:09 +0200

    Index

---
+++ JavaScript/cycle.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+
+const portfolioPictures =
+    [
+        { name: "Animals", href: `Portfolio/Pictures/Animals.jpg` },
+        { name: "Architecture", href: `Portfolio/Pictures/Architecture.jpg` },
+        { name: "Landscapes", href: `Portfolio/Pictures/Landscapes.jpg` },
+        { name: "Moon", href: `Portfolio/Pictures/Moon.jpg` },
+        { name: "Nature", href: `Portfolio/Pictures/Nature.jpg` },
+        { name: "Portraits", href: `Portfolio/Pictures/Portraits.jpg` },
+        { name: "Sun", href: `Portfolio/Pictures/Sun.jpg` },
+    ];
+
+const displayPic = document.getElementById("display");
+
+let x = 0;
+
+export function initialise() {
+    displayPic.src = (portfolioPictures[x].href);
+    setInterval(updateImages(), 5000);
+}
+
+
+
+function updateImages() {
+    x++;
+    if (x >= 7) {
+        x = 0;
+    }
+    displayPic.src = (portfolioPictures[x].href); 
+
+}
